e8feaba52c7b8fb8d71b037810cf93ae9d6b9638
Aquibshaikh313 <105275742+Aquibshaikh313@users.noreply.github.com>
2024-04-03 13:53:10 +0000

    config changes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,14 @@
+// let score = "Aquib"
+// console.log (typeof score);
+// console.log (typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log (typeof valueInNumber);
+// console.log(valueInNumber);
+
+let someNumber = "42";
+let stringNumber = String(someNumber);
+console.log(someNumber);
+console.log(typeof someNumber);
+
+ 

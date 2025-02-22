d5cf60a0687b62b92618e0f452ee55b1364300d2
Aquibshaikh313 <105275742+Aquibshaikh313@users.noreply.github.com>
2025-02-22 10:45:10 +0000

    conversion of data types

--- Day1/01_basics.js
+++ Day1/01_basics.js
@@ -1,1 +1,17 @@
 console.log("Aquib shaikh");
+
+let score = "27abc";
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+
+ let isLoggedIn = "";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true, 0=>false, ""=>false , "hi"=>true
+
+

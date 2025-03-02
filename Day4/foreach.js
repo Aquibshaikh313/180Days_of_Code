dd9ee887ad83f6a24250efe41592fff33bc280eb
Aquibshaikh313 <105275742+Aquibshaikh313@users.noreply.github.com>
2025-03-02 11:35:04 +0530

    Update foreach.js
    config done

--- Day4/foreach.js
+++ Day4/foreach.js
@@ -15,3 +15,20 @@ coding.forEach(val => {
     // console.log(val);
     
 });
+
+//Problems for practice:
+
+const num = [1, 2, 3, 4];
+let val = 0;
+num.forEach((num) => {
+  return (val += num);
+});
+// console.log(val);
+
+const arrs = [1, 2, , 4];
+let numcall = 0;
+arrs.forEach((element) => {
+  console.log({element});
+  numcall++;
+});
+console.log(numcall);

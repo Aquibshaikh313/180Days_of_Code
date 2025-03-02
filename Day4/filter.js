6ac8533b34ae8d23df16c9ffc5b0dbcd7e634ccd
Aquibshaikh313 <105275742+Aquibshaikh313@users.noreply.github.com>
2025-03-02 11:56:48 +0530

    Update filter.js
    config done

--- Day4/filter.js
+++ Day4/filter.js
@@ -20,3 +20,35 @@ const evenNums = myNums.filter((num) => num % 2 == 0);
 // console.log(evenNums);
 const oddNums = myNums.filter ((num)=> num % 2 !==0) //for odd
 console.log(oddNums);
+
+//***********************Problems for practise**************************************
+const words = ["spray", "elite", "exuberant", "destruction", "present"];
+
+const newWords = words.filter((item) => item.length > 5 );
+// console.log(newWords);
+
+const courses = [
+  {
+    courses : "html",
+    prices : 200
+
+  },
+  {
+    courses : "css",
+    prices : 300
+
+  },
+  {
+    courses : "js",
+    prices : 450
+
+  },
+  {
+    courses : "react",
+    prices : 600
+
+  }
+
+]
+const purchase = courses.filter((courses)=> courses.prices > 400  );
+console.log(purchase);

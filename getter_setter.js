d982c78d0e6f6ecd14b119d8075a2ef9dab0c219
Aquibshaikh313 <105275742+Aquibshaikh313@users.noreply.github.com>
2025-04-13 12:05:17 +0530

    Add files via upload

---
+++ getter_setter.js
@@ -0,0 +1,74 @@
+//class based getter setter
+
+class User {
+    constructor(email,password){
+        this.email = email;
+        this.password = password
+    }
+
+    get email(){
+         return this._email.toUpperCase();
+    }
+    set email(value){
+        this._email = value
+    }
+
+    get password(){
+        return `${this._password}hitesh`
+ 
+    }
+    set password(val){
+        this._password = val;
+    }
+}
+
+const hitesh = new User ('hitesh@gmail.com','123')
+console.log(hitesh.password);
+console.log(hitesh.email);
+
+//function based getter setter
+
+function User(email, password){
+    this._email = email;
+    this._password = password
+
+    Object.defineProperty(this, 'email', {
+        get: function(){
+            return this._email.toUpperCase()
+        },
+        set: function(value){
+            this._email = value
+        }
+    })
+    Object.defineProperty(this, 'password', {
+        get: function(){
+            return this._password.toUpperCase()
+        },
+        set: function(value){
+            this._password = value
+        }
+    })
+
+}
+
+const chai = new User("chai@chai.com", "chai")
+console.log(chai.email);
+
+//object based getter setter
+
+const User = {
+    _email: 'h@hc.com',
+    _password: "abc",
+
+
+    get email(){
+        return this._email.toUpperCase()
+    },
+
+    set email(value){
+        this._email = value
+    }
+}
+
+const tea = Object.create(User)
+console.log(tea.email);

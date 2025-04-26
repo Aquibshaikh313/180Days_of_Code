8eb20d0dd04fea78d8bc7e5ace09add092b6ff13
Aquibshaikh313 <105275742+Aquibshaikh313@users.noreply.github.com>
2025-04-26 19:53:29 +0530

    Update map.js

--- Day3/map.js
+++ Day3/map.js
@@ -1,4 +1,6 @@
-//Map create new array
+//map creates new array
+//Does not change the original array
+
 let lengths = ["html","css","javascript"];
 const totalLength =lengths.map(item => item.length);
 // console.log(totalLength);
@@ -11,5 +13,32 @@ const doubleNum = num.map((item)=> item*2);
 let number = [1,2,3,4,5,6,7,8];
 const evenNums = number.map((num) => num%2 == 0 ? num : null );
 const filterEvenNum = evenNums.filter((num) => num !== null);
-console.log(filterEvenNum);
+// console.log(filterEvenNum);
+
+const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+
+// const newNums = myNumbers.map((num) => {
+//   return num + 10;
+// });
+// console.log(newNums);
+
+// const newNume = myNumbers.map((num)=>{
+//   return num*2;
+// })
+// console.log(newNume);
+
+//Real world example of map:
+const pricesInDollar = [10,20,30];
+const pricesInRupees = pricesInDollar.map((num)=>{
+    return num*83;
+})
+console.log(pricesInRupees);
+
+const fruits = ['Apple', 'Banana', 'cherry'];
+const fruitList = fruits.map((val ,idx,arr)=>{
+    return `${idx+1} of ${arr.length} is ${val}`
+})
+console.log(fruitList);
+
 

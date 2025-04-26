16f2fca73f948e34a18a3dfef49d4ea08ecc5c84
Aquibshaikh313 <105275742+Aquibshaikh313@users.noreply.github.com>
2025-04-26 16:50:10 +0530

    Update if_statement.java

--- Day10_java_webdev/if_statement.java
+++ Day10_java_webdev/if_statement.java
@@ -3,7 +3,7 @@
 import java.util.*;
 public class Main {
   public static void main(String[] args) {
-      Scanner scn = new Scanner(System.in);
+      // Scanner scn = new Scanner(System.in);
       // int age = scn.nextInt();
       // if(age>=18){
       //   System.out.print("person is eligible to vote");
@@ -42,17 +42,75 @@ public class Main {
       // System.out.print(a+b);
 
       //Quiz:
-      int a = 50,b=50;
-      if(5 > 4){
-        System.out.println("first if");
+      // int a = 50,b=50;
+      // if(5 > 4){
+      //   System.out.println("first if");
        
 
-      }
-      if(10>=6){
-        System.out.println("second if");
+      // }
+      // if(10>=6){
+      //   System.out.println("second if");
       
         
-      }
+      // }
+
+      // Scanner scn = new Scanner(System.in);
+      // int age = scn.nextInt();
+      // if(age>=65){
+      //   System.out.print("Retired");
+      // }else{
+      //   System.out.print("Not retire");
+      // }
+
+      // Scanner scn = new Scanner(System.in);
+      // int num = scn.nextInt();
+
+      // if(num%2==0){
+      //   System.out.print("It is even number");
+      // }else{
+      //    System.out.print("It is even odd");
+      // }
+
+      // Checks number divisible by 5
+      // Scanner scn = new Scanner(System.in);
+      // int num = scn.nextInt();
+      // if(num % 5 == 0){
+      //   System.out.print("Divisible by 5");
+      // }else{
+      //    System.out.print("Not divisible by 5");
+      // }
+
+      // Checks number divisible by 2 or 3
+      // Scanner scn = new Scanner(System.in);
+      // int n = scn.nextInt();
+      // if(n%2==0 || n%3==0){
+      //   System.out.print("number is divisible by 2 or 3 ");
+      // }else{
+      //   System.out.print("number is not divisible");
+      // }
+
+      // if(n%2==0){
+      //   System.out.print("number is divisible by 2 ");
+
+      // }
+      // else if(n%3 == 0){
+      //    System.out.print("number is divisible by 3 ");
+
+      // }
+      // else{
+      //   System.out.print("number is not divisible");
+      // }
       
+      //print 2 numbers in that find max of 2 numbers
+      Scanner scn = new Scanner(System.in);
+      int a = scn.nextInt();
+      int b = scn.nextInt();
+      if(a > b ){  // if we take 10 10 i.e 10 > 10 it is false 
+        System.out.print("a");
+      }else{
+        System.out.print("b");//therefore it will print b
+      }
+
+          
   }
 }

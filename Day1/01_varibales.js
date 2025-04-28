63e92d71c6d9ecc17c31841e3ffebeeaea4403d8
Aquibshaikh313 <105275742+Aquibshaikh313@users.noreply.github.com>
2025-04-28 10:10:35 +0530

    Update 01_varibales.js

--- Day1/01_varibales.js
+++ Day1/01_varibales.js
@@ -1,11 +1,36 @@
-let name = "Aquib"; //Even without the semicolon, JavaScript inserts it
-// console.log(name);//Aquib
+// Rules for naming variable in javascript
+//1. Cannot start with number 
+//2. Cannot use keywords like var, let, const ,function etc as variable name
+//3. it starts with _ or $ or letter 
+//4. Can use numbers in between or at end of variable name but not at the start
+//5.Cannot use space in between variable name
+//6. Case sensitive means name and Name are different variables 
+
+let Name = "Aquib"; 
+// console.log means print in console.
+//Even without the semicolon, JavaScript inserts it
+console.log(Name);//Aquib
 // console.log("name");//name
 
-let scores = "27abc";
-console.log(typeof(scores));
+// console.log(surname);//cannot access before initialization
+let surname = "khan";
 
-const accountId = 144553
+let scores = "27abc";
+// console.log(typeof(scores));
+
+
+let x = true;
+// console.log(typeof(x));
+console.log(typeof(Boolean));
+//boolean is a fuction in javascript
+console.log(typeof(String));
+//string is a function in js
+// Data types are basically funcitons in javascript
+//console.log(typeof(Number));//function
+console.log(typeof(Object));
+
+
+const accountId = 144553;//const is a constant variable cannot be changed/reassigned
 let accountEmail = "hitesh@google.com"
 var accountPassword = "12345"
 accountCity = "Jaipur"
@@ -16,9 +41,12 @@ let accountState;
 
 accountEmail = "hc@hc.com"
 accountPassword = "21212121"
-accountCity = "Bengaluru"
+accountCity = "Bengaluru"//s
 
-console.log(accountId);
+
+// console.log(accountId);
+//we dont use var in modern javascript because of block scope and functional scope
+//it is a functional scope variable
 
 /*
 Prefer not to use var
@@ -26,4 +54,11 @@ because of issue in block scope and functional scope
 */
 
 
-console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+// console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/*
+- We avoid using `var` in modern JavaScript.
+- Because `var` is function-scoped, not block-scoped → can cause issues.
+- `let` and `const` are block-scoped → safer and more predictable.
+- Prefer using `const` by default, and use `let` if value needs to change.
+*/

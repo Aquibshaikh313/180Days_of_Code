606efb0b9bf85394c14b8095bfa93216ad356107
Aquibshaikh313 <105275742+Aquibshaikh313@users.noreply.github.com>
2025-04-29 21:53:05 +0530

    Update 05_strings.js
    Includes cheatsheet for string

--- Day1/05_strings.js
+++ Day1/05_strings.js
@@ -146,3 +146,58 @@ console.log(str.endsWith('World'));             // true
 // padStart() and padEnd() --> for padding
 console.log('5'.padStart(4, '0'));              // 0005
 console.log('5'.padEnd(4, '0'));                // 5000
+
+
+
+
+
+
+
+
+// Cheatsheet for String****************
+let str = "Hello World";
+
+str.length                  // 11
+str.charAt(0)               // 'H'
+str.indexOf('o')            // 4
+str.includes('World')       // true
+str.toUpperCase()           // "HELLO WORLD"
+str.toLowerCase()           // "hello world"
+str.trim()                  // removes spaces from start and end
+
+tr.slice(0, 5)             // "Hello"
+str.slice(-5)               // "World"
+str.substring(0, 5)         // "Hello" (no negative allowed)
+
+str.replace('World', 'JS')  // "Hello JS"
+str.startsWith('Hell')      // true
+str.endsWith('World')       // true
+
+str.split(' ')              // ['Hello', 'World']
+['Hello', 'World'].join(' ')// "Hello World"
+"Hello".concat(" ", "World")// "Hello World"
+
+let name = "John";
+console.log(`Hello, ${name}!`) // Hello, John!
+
+let num = 1234567;
+num.toLocaleString('en-IN') // "12,34,567" (Indian format)
+
+"5".padStart(3, '0')        // "005"
+"5".padEnd(3, '0')          // "500"
+
+"ha".repeat(3)              // "hahaha"
+
+let strNum = "123";
+Number(strNum)              // 123
+String(123)                 // "123"
+
+str.substr(0, 5)            // "Hello" (old, not recommended)
+str.__proto__               // String.prototype (access properties)
+Object.getPrototypeOf(str)  // Better way than __proto__
+
+// Custom Method Example:
+String.prototype.trueLength = function() {
+    return this.trim().length;
+};
+console.log("  hello  ".trueLength());  // 5

847e70eb3b1e147e2cb471a63a720947d7a2959b
Aquibshaikh313 <105275742+Aquibshaikh313@users.noreply.github.com>
2025-05-02 00:36:17 +0530

    Add files via upload

---
+++ Day3/03_whileLoop.js
@@ -0,0 +1,79 @@
+// //while loop 
+// let i = 0;
+// while (i<=5) {
+//     // console.log(`value of index is ${i}`);
+//     i++;
+    
+// }
+
+//Even number using while loop
+// let num = 2;
+// while (num<=10){
+//   if(num%2==0){
+//     console.log(num);
+//   }
+//   num++;
+// }
+
+// printing Even numbers from 10 to 1 reverse order:
+// let num = 10;
+// while(num>=0){
+//   if(num%2==0){
+//     console.log(num);
+//   }
+//   num--;
+// }
+
+// odd number by using while loop
+// let num =1;
+// while(num<=10){
+//   if(num%2!==0){
+//     console.log(num);
+//   }
+//   num++;
+// }
+
+// printing odd numbers from 10 to 1 reverse order:
+// let num = 10;
+// while(num>=1){
+//   if(num%2!== 0){
+//     console.log(num);
+
+//   }
+//   num--;
+// }
+
+//Countdown 10 to 1:
+// let a = 10;
+// while(a>=1){
+//     console.log(a);
+//     a--;
+// }
+
+//sum of numbers 1 to 100
+// let sum = 0;
+// let current = 1;
+// while(current<=100){
+//         sum+=current;
+//         current++;
+    
+// }
+// console.log("Sum:", sum);//5050
+
+
+// let i = 2;
+// // let n = prompt("enter the number");
+// while(i<=10){
+//   console.log(i);
+//   i+=2;
+// }
+
+
+// let myArray = ["flash","Batman","superman"]
+
+// let arr = 0
+// while (arr < myArray.length){
+//     console.log(`value of ${myArray[arr]}`);
+//     arr++;
+    
+// }

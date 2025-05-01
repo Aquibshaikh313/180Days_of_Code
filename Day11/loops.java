5c437fa0f0a85a95533f1b21c743cfc0a20bb334
Aquibshaikh313 <105275742+Aquibshaikh313@users.noreply.github.com>
2025-05-01 19:09:41 +0530

    Update loops.java

--- Day11/loops.java
+++ Day11/loops.java
@@ -55,11 +55,91 @@ public class Main {
         
       // }
 
-    //   int i = 2;
-    //   while(i<=n){
-    //     System.out.print(i);
-    //     i = i + 2;
-    //   } //no of iteration n/2
+      // int i = 2;
+      // while(i<=n){
+      //   System.out.print(i);
+      //   i = i + 2;
+      // }
+
+      //******************Assignment******************
+      // Scanner scn = new Scanner(System.in);
+      // int n = scn.nextInt();
+      // int i = 1;
+
+      // while(i<=n){
+      //   System.out.print(i + " ");
+      //   i++;
+      // }
+      // Scanner scn = new Scanner(System.in);
+      // int i = scn.nextInt();
+
+      // while(i>=1){
+      //   System.out.print(i + " ");
+      //   i--;
+      // }
+
+      //  Scanner y = new Scanner(System.in);
+      //   int u = y.nextInt();
+
+      //   int n = 1;
+      //   while(n<=u){
+      //       if(n%2==0){
+      //       System.out.print(n + " ");
+      //       }
+      //       n++;
+      //   }
+
+      // Scanner scn = new Scanner(System.in);
+      // int N = scn.nextInt();
+
+      // int i = 4;
+      // while(i<N){
+      //   if(i%4==0){
+      //       System.out.print(i + " ");
+      //   }
+      //   i+=4;
+
+      // }
+
+      // Scanner scn = new Scanner(System.in);
+      // int N = scn.nextInt();
+      // int i = 1;
+      // while(i<=N){
+      //   if(i%2!=0){
+      //     System.out.print(i + " ");
+      //   }
+      //   i+=2;
+      // }
+
+
+      // Assignment for even from 5 to 1
+      //  Scanner scn = new Scanner(System.in);
+      // int i = scn.nextInt();
+
+      // while(i>=0){
+      //  if(i%2==0){
+      //      System.out.print(i + " ");
+      //  }
+      //   i--;
+      // }
+
+      // Assignment for odd from 5 to 1
+      //  Scanner scn = new Scanner(System.in);
+      // int i = scn.nextInt();
+
+      // while(i>=0){
+      //  if(i%2 !=0){
+      //      System.out.print(i + " ");
+      //  }
+      //   i--;
+      // }
+
+      
+
+
+       
+
+
 
 
   }

31d543bf64d2a39f575782f4bfddae25ab04c957
Aquibshaikh313 <105275742+Aquibshaikh313@users.noreply.github.com>
2025-05-01 19:09:18 +0530

    Add files via upload

---
+++ Day11/loops.java
@@ -0,0 +1,66 @@
+import java.util.*;
+public class Main {
+  public static void main(String[] args) {
+      
+      // int i = 1;
+      // while(i>=5){ //false since i = 1;
+      //   System.out.print(i + " ");
+      //   i= i+1;
+      // }
+
+      // int i = 1;
+      // while(i<=3){
+      //   System.out.print(i + " ");
+      // }
+
+      // int i = 1;
+      // while(i<5){
+      //   System.out.print(i + " ");
+      //   i++;
+      // }
+
+      // int i = 0;
+      // while(i<=10){
+      //   System.out.print(i + " ");
+      //   i++;
+      // }
+
+      // int i = 0;
+      // while(i>=10){
+      //   System.out.print(i + " ");
+      //   i++;//Nothing will be printed
+      // }
+
+      // int i = 0;
+      // while(i<=10){
+      //   System.out.print(i + " ");
+      //   i = i * 2;
+      // }//infinite loop
+
+       // int i = 0;
+      // while(i<=5){
+      //   System.out.print(i + " ");
+      //   i = i - 1;
+      // }//infinite loop
+      
+      // Scanner scn = new Scanner(System.in);
+      // int n = scn.nextInt();
+
+      // int i = 2;
+      // while(i<=n){
+      //   if(i%2==0){
+      //     System.out.println(i);
+      //   }
+      //   i+=2;
+        
+      // }
+
+    //   int i = 2;
+    //   while(i<=n){
+    //     System.out.print(i);
+    //     i = i + 2;
+    //   } //no of iteration n/2
+
+
+  }
+}

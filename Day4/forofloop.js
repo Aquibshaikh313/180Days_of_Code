801d4f52c5fe066c867ea232c77a440ca820f6d8
Aquibshaikh313 <105275742+Aquibshaikh313@users.noreply.github.com>
2025-06-08 23:16:49 +0530

    Update forofloop.js

--- Day4/forofloop.js
+++ Day4/forofloop.js
@@ -1,5 +1,5 @@
 //for of loop
-
+//for..of is basically used in arrays,map,string,set,nodelist,typedarray,arguments objects, generators produced by generator functions, and user-defined iterables.
 const arr = [1, 2, 3, 4, 5];
 
 for (const val of arr) {
@@ -8,18 +8,35 @@ for (const val of arr) {
 
 const greeting = "Hello World";
 for (const greet of greeting) {
-//   console.log(`Each char has ${greet}`);
+    console.log(`Each char has ${greet}`);
 }
 
-//Maps in for of loop
+//Using object.enteries...
+const user = {
+  name: "Aman",
+  age: 22
+};
 
-const myObject = {
-    "name": "Aquib",
-    "age":26,
-    "country": "India",
+for (let [key, value] of Object.entries(user)) {
+  console.log(`${key}: ${value}`);
 }
 
-for (const key of myObject) {
-    console.log(key);
-    
+
+//  for...of with Objects (❌ Error if used directly)
+const obj = { a: 1, b: 2 };
+
+for (let item of obj) {
+  console.log(item);  // ❌ Error: obj is not iterable
 }
+
+//Fix
+for (let key of Object.keys(obj)) {
+  console.log(key); // a, b
+}
+
+for (let value of Object.values(obj)) {
+  console.log(value); // 1, 2
+}
+
+
+

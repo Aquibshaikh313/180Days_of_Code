83a4c4bca0c2c131f82bb4015d65094ff2fb0c18
Aquibshaikh313 <105275742+Aquibshaikh313@users.noreply.github.com>
2025-07-03 20:48:53 +0530

    Update promises.js

--- Day7/promises.js
+++ Day7/promises.js
@@ -1,36 +1,59 @@
-// const promiseOne = new Promise(function (resolve, reject) {
-//   setTimeout(function () {
-//     console.log("Async task complete");
-//     resolve(); // now its connected to then
-//   }, 1000);
-// });
-// promiseOne.then(function () {
-//   console.log("promise consumed");
-// });
+// --------------------------
+// 1. Basic Promise Example
+// --------------------------
 
-// //Without storing in variable:
-// new Promise(function (resolve, reject) {
-//   setTimeout(function () {
-//     console.log("Async task 2 complete");
-//     resolve(); //for connecting to then
-//   }, 1000);
-// }).then(function () {
-//   console.log("Promise2 consumed ");
-// });
+// Creating a promise and resolving it after 1 second
+const promiseOne = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    // Simulating an asynchronous task
+    // console.log("Async task complete");
+    resolve(); // Resolve the promise
+  }, 1000);
+});
 
-// const promiseThree = new Promise(function (resolve, reject) {
-//   setTimeout(function () {
-//     resolve({ Username: "chai", Email: "chai256@gmail.com" });
-//   }, 1000);
-// });
+// Consuming the resolved promise using .then()
+promiseOne.then(function () {
+  // console.log("promise consumed");
+});
 
-// promiseThree.then(function (user) {
-//   console.log(user);
-// });
+
+// ------------------------------
+// 2. Inline Promise (No Variable)
+// ------------------------------
+
+// Creating and consuming a promise without storing it in a variable
+new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    console.log("Async task 2 complete");
+    resolve(); // Resolve the promise
+  }, 1000);
+}).then(function () {
+  // console.log("Promise2 consumed");
+});
+
+
+// -----------------------------
+// 3. Promise with Data (Object)
+// -----------------------------
+
+const promiseThree = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    resolve({ Username: "chai", Email: "chai256@gmail.com" }); // Sending data on resolve
+  }, 1000);
+});
+
+promiseThree.then(function (user) {
+  console.log(user); // Logs the resolved user object
+});
+
+
+// -------------------------------------
+// 4. Promise with then/catch/finally
+// -------------------------------------
 
 const promiseFour = new Promise(function (resolve, reject) {
   setTimeout(function () {
-    let error = false;
+    let error = false; // Change to true to test rejection path
     if (!error) {
       resolve({ username: "hitesh", password: "123" });
     } else {
@@ -42,28 +65,38 @@ const promiseFour = new Promise(function (resolve, reject) {
 promiseFour
   .then((user) => {
     console.log(user);
-    return user;
+    return user; // Passing to next .then
   })
   .then(({ username, password }) => {
-    console.log(username, password);
+    console.log(username, password); // Destructured output
   })
   .catch(function (error) {
-    console.log(error);
+    console.log(error); // Logs if promise is rejected
   })
   .finally(() => {
-    console.log("promiseFour either resolve or rejected");
+    console.log("promiseFour either resolved or rejected");
   });
-//Simple basic return example to unerstanding above
-// function getUser() {
-//     return { username: "hitesh", age: 25 };
-//   }
 
-//   let user = getUser();
-//   console.log(user.username, user.age);  // Output: hitesh 25
+
+// --------------------------------------
+// 5. Basic Return Function (Sync Example)
+// --------------------------------------
+
+function getUser() {
+  return { username: "hitesh", age: 25 };
+}
+
+let user = getUser();
+console.log(user.username, user.age);  // Output: hitesh 25
+
+
+// ----------------------------------------
+// 6. Async/Await with Promise (Try/Catch)
+// ----------------------------------------
 
 const promiseFive = new Promise(function (resolve, reject) {
   setTimeout(function () {
-    let error = true;
+    let error = true; // Change to false to test resolve path
     if (!error) {
       resolve({ username: "javascript", password: "123" });
     } else {
@@ -82,26 +115,34 @@ async function consumePromiseFunction() {
 }
 consumePromiseFunction();
 
-// async function getAllUsers() {
-//   try {
-//     const response = await fetch(
-//       "https://jsonplaceholder.typicode.com/users"
-//     );
-//     const data = await response.json();
-//     console.log(data);
 
-//   } catch (error) {
-//     console.log("E:", error);
-//   }
-// }
-// getAllUsers();
+// ------------------------------------------
+// 7. Fetch Users with Async/Await (API Call)
+// ------------------------------------------
 
-//Now by using then & catch
+async function getAllUsers() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json(); // Parsing JSON
+    console.log(data); // Logs array of users
+  } catch (error) {
+    console.log("E:", error); // Logs if fetch fails
+  }
+}
+getAllUsers();
+
+
+// -----------------------------------------------------
+// 8. Fetch Users using .then() and .catch() (API Call)
+// -----------------------------------------------------
+
 fetch("https://jsonplaceholder.typicode.com/users")
   .then((response) => {
-    return response.json();
+    return response.json(); // Parse JSON from response
   })
   .then((data) => {
-    console.log(data);
+    console.log(data); // Logs array of users
   })
-  .catch((error) => console.log(error));
+  .catch((error) => {
+    console.log(error); // Logs if an error occurs
+  });

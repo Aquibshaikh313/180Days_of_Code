a7a9f2ac26cc32dcba14d00768230eee832f0e54
Aquibshaikh313 <105275742+Aquibshaikh313@users.noreply.github.com>
2025-07-05 21:40:29 +0530

    Add files via upload

---
+++ Day14/Function2.java
@@ -0,0 +1,56 @@
+// // import java.lang;
+// public class Main {
+//   public static void test2() {
+// //   // static double areaofcirle(int R){
+// //   //  double area = Math.PI * R * R ;
+
+// //   //  return area;
+
+// //   // }
+//   System.out.println("Hi");
+//   return ;
+
+// }
+
+
+//   public static void main(String[] args) {
+// //     // Scanner scn = new Scanner(System.in);
+// //     // int radius = scn.nextInt();
+
+// //     // double area = areaofcirle(radius);
+// //     // System.out.print(area);
+//     test2();
+//     System.out.println("hello");
+      
+//   }
+// }
+
+// //Example 2 :
+// // public class Main {
+// // public static boolean isEven(int N){
+// //   if(N%2==0){
+// //     return true ;
+// //   }else{
+// //     return false;
+// //   }
+ 
+// // }
+// // public static void main(String[] args){
+// //   System.out.println(isEven(60));
+// //   System.out.println("Hello");
+// // }
+// // }
+
+public class Main {
+  public static void EvenOdd(int N){
+    if(N%2 == 0){
+      System.out.println("Even");
+      return;
+    }
+    System.out.println("odd");
+
+}   public static void main(String [] args){
+    EvenOdd(10);
+    System.out.println("hello");
+   }
+}

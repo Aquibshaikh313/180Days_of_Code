29cbfa13c34cbce0768ff7dd3bd69b9b96a3a635
Aquibshaikh313 <105275742+Aquibshaikh313@users.noreply.github.com>
2025-07-08 17:03:20 +0530

    Add files via upload

---
+++ Day 15/String1.java
@@ -0,0 +1,46 @@
+
+// public class Main {
+//   public static void main(String[] args) {
+//   //  int a = 'A';
+//   //  System.out.print(a);//65
+//     // System.out.println((char)65);//A  implicit typecasting
+
+//   //  char a = 'A';
+//   //  System.out.println(a); //A
+//   // if(ch>= 90){
+//   //   System.out.println("Greater");
+//   //     }else{
+//   //       System.out.println("Smaller");
+//   //     }
+     
+
+//   //    if('a'>'A'){
+//   //   System.out.println("true");
+//   // }else{
+//   //   System.out.println("false");
+//   // }
+//   String str = "sanjay shettigar";
+//   // System.out.print(str.charAt(3));//j
+//   // System.out.print(str.length()); //6 counts space also
+  
+// //substring
+// // startIndex is inclusive → it starts from this index.
+// // endIndex is exclusive → it stops before this index.
+//   // System.out.print(str.substring(2,5));//nja
+//   String name = "Aquib shaikh";
+//     // System.out.print(name.substring(8));//
+//     System.out.print(name.substring(6,name.length()));
+//      String name = "Aquib";
+//     // System.out.print(name.substring(8));//
+//     // System.out.print(name.substring(6,name.length()));
+//     // System.out.println(name.indexOf("shaikh",3));
+// for(int i=0; i<name.length(); i++){
+//   System.out.println("character at " + i + " is : " + name.charAt(i));
+// }
+
+
+
+//   }
+  
+  
+// }

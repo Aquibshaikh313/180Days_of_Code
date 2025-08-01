502c9d7921522f98877dacdc4a15c697247ebdc7
Aquibshaikh313 <105275742+Aquibshaikh313@users.noreply.github.com>
2025-08-01 21:48:53 +0530

    Update 2D array3.java

--- Day17/2D array3.java
+++ Day17/2D array3.java
@@ -156,3 +156,73 @@ public class Main {
         }
     }
 }
+
+//************************Printing Diagonal ELements in matrix"*********************
+import java.util.*;
+public class Main {
+  
+  static void diagElem(int[][] mat){
+    int n = mat.length; //since its a square matrix
+    for(int i = 0 ; i < n ; i++){
+      System.out.println(mat[i][i]);
+    }
+    return;
+  }
+  public static void main(String[] args) {
+      int[][] mat = 
+      {
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+      };
+
+      diagElem(mat);
+  }
+}
+
+//********************************Adding two Different matrices********************************
+import java.util.*;
+public class Main {
+  
+  static int[][] diagElem(int[][] A, int[][] B){
+    int n = A.length; //since its a square matrix
+    int m = A[0].length;
+
+    int[][] C = new int[n][m];
+   
+    for(int i = 0 ; i < n ; i++){
+      for(int j = 0 ; j<m ;j++){
+        C[i][j] = A[i][j]  + B[i][j] ;
+      }
+    }
+    return C;
+    
+  }
+  public static void main(String[] args) {
+      int[][] A = 
+      {
+        {1,2},
+        {3,4},
+        {5,6}
+      };
+      int[][] B = 
+      {
+        {1,1},
+        {1,1},
+        {1,1}
+      };
+      
+
+
+    int[][] result = diagElem(A, B);
+
+        // Print the result properly
+        System.out.println("Sum of matrices:");
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[0].length; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+  }
+}

34acb42c032d8d4636bbd391ae3b9d89fc6e8069
Aquib Shaikh <105275742+Aquibshaikh313@users.noreply.github.com>
2025-08-05 20:33:55 +0530

    Add files via upload
    Started building textUtil website, added nav completed props topic today

---
+++ Day18/01vitereact/src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar";
+
+
+function App() {
+  return (
+    <>
+    <Navbar title = "Funky" abouttxt="About"/>
+    
+     {/* <Navbar/> */}
+      
+    </>
+  );
+}
+
+export default App;
+
